1c5dadeb8231eac4f570f7c5037a988f58897590
Tom Leonard <tleonard@plaid.com>
2022-06-05 22:54:19 -0400

    adding deliverable 2 code and csv

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Create a table of employees retiring
 SELECT e.emp_no, 
 e.first_name, 
 e.last_name,
@@ -22,8 +23,27 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
+--Create table with count of titles of employees retiring
 SELECT COUNT (title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC
+
+--Creating mentorship eligibility table
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no
